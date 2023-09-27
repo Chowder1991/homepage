9ff648ea3032183fb37aa746a6484fd739a50118
Lukasz Kucinski <kucinskilukas@gmail.com>
2023-09-27 15:31:19 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+console.log("Cześć Wtam")
+
+let button0 = document.querySelector(".button0");
+let header = document.querySelector(".header");
+
+button0.addEventListener("click", () => {
+    header.remove();
+});
+
+let button = document.querySelector(".button");
+let image = document.querySelector(".image");
+
+button.addEventListener("click", () => {
+    image.remove();
+});
+
+let button1 = document.querySelector(".button1");
+let image1 = document.querySelector(".image1");
+
+button1.addEventListener("click", () => {
+    image1.remove();
+});
+
+let button2 = document.querySelector(".button2");
+let image2 = document.querySelector(".image2");
+
+button2.addEventListener("click", () => {
+    image2.remove();
+});
+
+let changeColorButton = document.querySelector(".changeBackground");
+let container = document.querySelector(".container");
+
+changeColorButton.addEventListener("click", () => {
+    container.classList.toggle("whiteBackground")
+});
